45114e8aa042e2570905341d70872e95d281b40b
Nya Tyus <nyastyus@gmail.com>
2023-12-27 23:15:18 -0500

    create database connection file

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+
+//creates connection to the database
+const db = mysql.createConnection(
+  {
+    //Apple M1 chip host
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "expenses_db",
+  },
+  console.log("Connected to the database.")
+);
+
+module.exports = db;
