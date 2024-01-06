ea11f014f61b9eded8f6e04188e5a8fc60c5e479
Nya Tyus <nyastyus@gmail.com>
2024-01-06 00:36:07 -0500

    install inquirer

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import inquirer
 
 db = mysql.connector.connect(
     host="localhost",
@@ -8,3 +9,13 @@ db = mysql.connector.connect(
 )
 
 cursor = db.cursor()
+
+questions = [
+    inquirer.List(
+        "main",
+        message="What would you like to do?",
+        choices=["View all trips","View all categories","View all expenses","Add a trip","Add an expense","Update a trip","Update an expense"],
+    ),
+]
+
+user_answer = inquirer.prompt(questions)
