0c5cff8a6f534ad4ababf650faa464529f266ec2
Nya Tyus <nyastyus@gmail.com>
2024-01-07 17:51:24 -0500

    handle view all trips user selection

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ def viewTrips():
     sql = "SELECT * FROM trips"
     db.cursor.execute(sql)
     trips = db.cursor.fetchall()
-    print(trips)
-    chooseOption()
+    # use pandas to format as table?
+    for trip in trips:
+        print(trip)
+    
+    userAnswer = inquirer.prompt(questions['ViewTrips'])
+    if userAnswer["all_trips"] == "Select a trip":
+        selectOneTrip()
+    elif userAnswer["all_trips"] == "Remove a trip":
+        removeTrip()
+    else:
+        chooseOption()
 
 def viewCategories():
     print("hi")
@@ -18,6 +27,13 @@ def addTrip():
     print("hello")
     chooseOption()
 
+def selectOneTrip():
+    print('yooo')
+    chooseOption()
+
+def removeTrip():
+    print("bye")
+    chooseOption()
 
 def chooseOption():
     userAnswer = inquirer.prompt(questions['Main_Q'])
